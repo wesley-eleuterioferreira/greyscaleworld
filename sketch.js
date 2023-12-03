6463007af7d68ba348b38856d622e4a1d6b37dca
wesley-eleuterioferreira <152800322+wesley-eleuterioferreira@users.noreply.github.com>
2023-12-03 11:04:55 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+//The setup function only happens once
+function setup() {
+	createCanvas(500, 500); //create a 500px X 500px canvas
+}
+ 
+function draw() {
+  background(0); 
+  fill(255);
+  rect(mouseX,mouseY,50,50,25);
+
+  
+  
+}
+function mousePressed() {
+  rect=rec+1;
+  }
+
+
